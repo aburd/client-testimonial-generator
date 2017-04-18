de437ebd492274590da301aaf79c309518ec15be
Aaron Burdick <burdick.aaron@gmail.com>
2017-04-18 18:53:43 +0900

    Initial commit

---
+++ eurasia-group.js
@@ -0,0 +1,68 @@
+const data = {
+  client: {
+    name: "Kei Sakaguchi",
+    title: "Country Representative",
+    company: "Eurasia Group",
+    product: "Serviced Office",
+    place: "Japan",
+    location: "Marunouchi Trust Tower - Main, Tokyo",
+    cover: "/media/26257/sakaguchi-cover.jpg",
+  },
+  article: [
+    {
+      interview: `> The decisive factor was the ability to set up immediately and do it on my own…
+
+# When did you first encounter Servcorp?
+## Prior to joining Eurasia Group, I belonged to an IT software company headquartered in San Francisco, USA. As the corporation was relatively young, when they entered the Japanese market initially they chose to borrow a Serviced Office and man it with a few local staff. After considering their options including taking up a Servcorp Serviced Office, I heard they went for a competitor, but in that process I heard what Servcorp also had to offer.
+
+# Why did you decide to move into a Servcorp Serviced Office this time?
+## While the Eurasia Group started its base in Japan from scratch, there were already 50 Japanese clients utilizing the company’s services so we already had a firm foothold. As business trips to New York are frequent, I needed to set up a base quickly and with as little fuss as possible. I also wanted to establish in an impressive location with quality services and facilities. We decided that Servcorp would be the best choice because they offered these things and we would be able to start immediately.
+
+# What was the deciding factor?
+## When we were making enquiries, Servcorp were quick, accurate and detailed in their responses. Also, since 70-80% of our clients were based in the local area of Marunouchi, the Trust Tower Main Building made sense. We made the judgement based on the high quality of the facilities, and the faultless service we received from the staff.
+
+# By choosing Servcorp, what problems were immediately solved?
+## First of all as I was establishing the office on my own, I didn’t need much space. It was wonderful that I had a complete package to operate as an office from Day 1, such as telephone, internet, secretarial services, and other facilities necessary to work.
+
+# What kind of services do you use at Servcorp?
+## We have our telephone calls answered and transferred as we like by the receptionist, and make good use of the bilingual secretarial services. I think that the work we need to dispatch to these secretaries will increase in the future. Because I have access to this business support when I am busy, it allows me peace of mind.
+
+# How do you want to use secretarial services in the future?
+## Since we may increase the number of employees in the future, I think the need for secretarial services will be different to the present time where I am working solo, but during busy periods where we need to urgently produce a large volume of materials or organize our database, I would like to request it.
+
+# What is your favorite thing about your Servcorp Office?
+## I can work independently, in my own bubble which allows me to work calmly and efficiently. The maintenance of facilities is also thorough and the pantry service is particularly substantial. Moreover, the quality of the staff is extremely high. Everyone is very professional. I can also rely on the staff to manage my calls that are critical to the business with absolute confidence.
+
+# Tell us about the line of work you are in?
+## Eurasia Group is a political risk consulting company established by political scientist Ian Bremer back in 1998. Our Head Office is located in New York, with about 140 employees worldwide. Approximately half of our employees analyze geopolitical risks around the world and provide analytical content to companies. Our basic service is to provide reports created by our analysts to our corporate clients. In some countries where regime change is common, especially in emerging countries, it can be difficult for companies to know whether foreign investment will have big impacts on the stability of their business in that country. More companies are wanting to comprehensively understand the issues before entering the country. In response to the needs of such companies, we provide these analytical services.
+
+# Why did you decide to establish yourselves in Japan?
+## We have about 300 clients worldwide, of which about 50 are Japanese. Until now, we had no offices in Japan, so employees at the New York Headquarters had to travel back and forth frequently to explore new business. As the number of clients in Japan has increased in recent years, we decided it best to establish a physical presence here. I was employed in June 2015, and started office in Tokyo the following month. Although we are still just beginning, we will increase by one soon and are seeking to increase again at the end of the year.
+
+# Are you planning to continue with Servcorp?
+## It depends on the extent of future expansion. When you look at it closely in terms of space and cost, it makes sense for the time being. It all depends on the speed and level of future expansion.
+
+# Do you interact much with other companies on your floor?
+## I deal mostly with the Servcorp staff. I know there are events but I have not been able to participate yet. I am however thankful to Servcorp for providing many opportunities for mutual exchange.
+
+# Do you have any particular wishes regarding Servcorp service or facilities in the future?
+## As I have to hold meetings regularly, I sometimes wish there were more than two small meeting rooms available. It would make reservations simpler.  Also, if the reception time could be extended by 30 minutes it would be great. I would be interested to know whether any flexibility of facilities and business hours could exist in the future.`,
+      pictures: ['/media/26094/sakaguchi-receptionist.jpg', ]
+    },
+    {
+      interview: `### Notes from Mr. Sakaguchi’s Receptionist, Erika-san
+
+# Main support work
+## Managing Mr Sakaguchi’s telephone calls, and preparation and printing of materials used for his meetings.
+
+# Things we pay particular attention to for Mr Sakaguchi
+## For Mr Sakaguchi we deal with a large number of calls from overseas, and also major domestic companies and as well as the media, so we must make a good impression over the phone. We pay close attention to the way we interact and add that necessary smile to our voices.
+
+# Thoughts on having Mr Sakaguchi as a client
+## Mr. Sakaguchi is always gentlemanly and has a nice smile so we always receive good energy at the reception desk when he passes by.
+
+# At Servcorp, we offer Serviced Offices with bilingual secretarial services ideal for companies that have business with overseas affiliates or those wishing to develop a base in Japan. Please do not hesitate to contact us. Click here for details!`,
+      pictures: ['/media/26095/sakaguchi-recpetionist-pose.jpg']
+    }
+  ]
+}
---
+++ value-creator.js
@@ -0,0 +1,78 @@
+const data = {
+  client: {
+    name: "Kiyohisa Konishi",
+    title: "Representative Director",
+    company: "Value Creation Corp",
+    product: "Serviced Office",
+    place: "Japan",
+    location: "Shinsaibashi Plaza Building - Shinkan, Osaka",
+    cover: "/media/26246/valuec-053.jpg",
+  },
+  article: [
+    {
+      interview: `# Mr. Kiyohisa Konishi, Representative Director of Value Creation Co Ltd., occupies a Servcorp Serviced Office on the 4th floor of the Shinsaibashi Plaza Building in Osaka. We talked to him about how he uses the office as well as how his business is growing as a result
+
+> The absolute empathy and trust I get from my secretaries is fantastic. It was an easy choice to upgrade from a Virtual Office to a Serviced Office.
+
+# When and how did you hear about Servcorp?
+## Before I chose to go with Servcorp, I was renting an office space on my own. It came about that I had to leave suddenly so I tried looking for alternatives like Virtual offices and Serviced Offices on the net. I selected three companies that I thought I would call and enquire further.
+
+# Why did you first choose to go with a Servcorp Virtual Office?
+## Among the three companies I found, the first one I called was Servcorp. I was struck by the politeness, responsiveness and general concern for my business needs. At that stage I was in the middle of moving and it was a difficult time. The voice on the other end seemed to understand my needs and so I decided to go and see an office that very same day.
+## This may have been a coincidence, but at the time, the Shinsaibashi location was on the last day of a campaign. It was a deciding factor that sealed the deal, but I have to say my mind was already made up at the phone call.&nbsp; In the end, I didnt even make enquiries to the two other companies I was originally planning to contact.
+
+# What kind of services do you use at Servcorp?
+## Since I am operating this company solo, I often have to go out so I use the receptionists a lot of manage my calls. Secretaries also take care of my mail, so it really does feel like they are my dedicated staff.
+## Mr Konishi laughs as he recalls how its not only him that benefits.
+## My clients are also extremely pleased. It is often the case where my clients will come for a meeting after having been away somewhere and bring souvenirs and gifts for the secretaries- but shockingly nothing for me!&nbsp; This is actually a pleasure for me to watch as it shows that the secretaries are really building strong relationships with not only me, but my clients as well.
+
+# What is your favorite thing about Servcorp?
+## The level of service is outstanding. Although I mostly use the receptionists services as I already explained, it is the care and manner taken at all times. My clients also love the service which assists me in maintaining a professional image for my business. If I was to employ a full-time highly skilled bilingual secretary, it would be way too costly. By using the services of Servcorp, I can drastically reduce such personnel expenses. So in reality, what I have with Servcorp is very cost effective and is a great deal.
+## Regarding the facilities, the office floor is very comfortable, but the boardroom is particularly impressive when I have to hold larger meetings.
+
+> The first word my clients always say when they use the boardroom for the first time is always "Wow!"
+
+# Do you often chat or engage with other companies on the same floor?
+## I have never worked with anyone else on my floor, but I greet everyone in passing etc. It is often the case that things build up from these simple hellos. I would like to one day if I find a business with similar objectives.
+
+# Do you use other Servcorp locations for business?
+## Yes actually, from March this year, I have set up a Tokyo branch office at Marunouchi Trust Tower. So far Ive only visited the office twice, but it is a lovely office and will no doubt give the great impression that I require for my customers.
+
+# Do you have any suggestions on how Servcorp can assist you further?
+## Previously, when I needed to register trademarks for the company, my patent attorney was introduced to me by a friend. It turned out that he was a Servcorp client at Edobori! &nbsp;We hit it off because of our Servcorp connection, so I decided to engage him after he gave me a special price. There is definitely a Servcorp effect. Servcorp should work on connecting their clients!
+
+(*We are happy to announce the launch of our <a href="http://www.servcorpcommunity.com">Community</a> which does exactly this!*)
+
+# Why did you choose a Virtual Office &amp; Servcorp Serviced Office rather than a standard rental office?
+## First of all, I decided to look for places that can handle my phone calls and provide a secretarial service. Call Management is not available when you go for a traditional rental space. As I have to go off site often, I cant handle my own calls in a timely and professional manner.
+
+# So why did you choose to upgrade from a Virtual Office to a Serviced Office?
+## When using the facilities at Shinsaibashi, I happened to see a vacant office and it was love at first sight! I decided to do my upmost to be able to rent it. Since securing the office, the necessity of having it has dramatically increased. With a Virtual Office, you have time limits in using the coworking facilities, and without a permanent space, you tend to have to lug around your baggage. My car was also packed with office related stuff. It was quite risky because at that time sales were not as good as they are now but I took on the challenge anyway. Now, I have no regrets and love the praise I get from my clients about the office.
+
+# Did you have any issues or problems you wanted to solve by upgrading?
+## As I mentioned earlier, it was largely due to limitations on using the coworking spaces and feeling like I was constantly carrying things around. These problems are now completely solved. I feel settled because I can work calmly any time - 365 days a year. Because of this, I will stay long term. I am also thankful to be able to see the cityscape of Shinsaibashi.
+
+# Which types of businesses do you think suit a Virtual Office and Serviced Office?
+## Sole practitioners and small business owners like myself who have to go off site on occasion.
+
+> The attractiveness is clear – with a Virtual Office you can have everything you need to run a small business effectively. Once space becomes an issue, you can transition to a Serviced Office.
+
+# Please tell us about your business!
+## We develop vital products for the paint and waterproofing construction industries. With our products, water leakage and paint peeling on constructions are drastically reduced, and we have a high level of client satisfaction. In addition, our products extend the life-span of buildings and reduce labor costs as we can shorten construction periods. However, what we excel in is not only selling the actual product, but also the construction method. We work with partners throughout the country, and our expertise is rapidly growing all over Japan.
+## You often hear about win-win situations, but in my case I like to use my own expression – happy-happy situations. If I am happy, everyone involved is happy. Thanks to Servcorp, I am able to work without hassle. I think my business model is not entirely different to Servcorps. In that respect, I think we are a perfect match!`,
+      pictures: ['/media/26249/valuec-032.jpg', '/media/26249/valuec-032.jpg']
+    },
+    {
+      interview: `### Introducing Mr Konishis Servcorp Secretary, Rika-san.
+
+# How do you assist Mr Konishi?
+## I have worked with Mr Konishi for about a year now. Previously, we were regularly sending out elevator inspections notices to specific addresses on behalf of Mr Konishi. More recently, we have arranged his business cards, managed his monthly invoices and completed some data entry on his behalf.
+
+> I really enjoy making him and his guests feel welcome in utilizing all Servcorp facilities.
+
+# What are the rewarding aspects of assisting a client like Mr Konishi?
+## I make a particular effort to remember his clients names and preferred beverages and respond with a smile. I get a kick when I hear from Mr Konishi that his clients left our floor extremely satisfied. If you visit our location, you will immediately see how Mr Konishi lights up the room. I am so happy to be able to continue to support his business, Value Creation Co., Ltd. into the future.`,
+      pictures: ['/media/26244/valuec-042.jpg']
+    }
+  ]
+}
